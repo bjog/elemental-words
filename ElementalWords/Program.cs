d77e5d534cd534e77644a1facda9a31d776cbdcf
Bhavish Jogeeah <bjogeeah@gmail.com>
2025-08-06 14:13:06 +0100

    Initial commit

---
+++ ElementalWords/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ElementalWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
