75c359a69a2e7de05d752f2283ad8c9ee9af34e6
Bhavish Jogeeah <bjogeeah@gmail.com>
2025-08-06 15:00:06 +0100

    Add stub implementation of FindElementalForms

---
+++ ElementalWords/ElementalWords.cs
@@ -0,0 +1,10 @@
+﻿namespace ElementalWords
+{
+    public static class ElementalWords
+    {
+        public static IEnumerable<IEnumerable<string>> FindElementalForms(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
