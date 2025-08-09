eb6b3b9ce61d58a47170e334566a4c8304634bba
Bhavish Jogeeah <bjogeeah@gmail.com>
2025-08-09 17:05:24 +0100

    Add interactive mode

--- ElementalWords/Program.cs
+++ ElementalWords/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Write("Input a word: ");
+                var input = Console.ReadLine();
+
+                var elementalForms = ElementalWords.FindElementalForms(input ?? string.Empty);
+
+                Console.WriteLine("Elemental Forms: ");
+
+                foreach (var elementalForm in elementalForms)
+                {
+                    Console.WriteLine(string.Join(", ", elementalForm));
+                }
+            }
         }
     }
 }
